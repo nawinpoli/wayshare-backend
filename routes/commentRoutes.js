3847b7f8dd55567953d4ef4086c25879eaed2f65
nawinpoli <kingjal15938@gmail.com>
2025-04-21 13:38:05 +0700

    แก้ deleteAll

--- routes/commentRoutes.js
+++ routes/commentRoutes.js
@@ -2,11 +2,12 @@ const express = require('express');
 const router = express.Router();
 const commentController = require('../controllers/commentController');
 
+router.delete('/deleteAll', commentController.deleteAllComments);
 router.get('/', commentController.getAllComments);
 router.get('/:id', commentController.getCommentById);
 router.post('/', commentController.createComment);
 router.put('/:id', commentController.updateComment);
 router.delete('/:id', commentController.deleteComment);
-router.delete('/deleteAll', commentController.deleteAllComments);
+
 
 module.exports = router;
--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -2,11 +2,12 @@ const express = require('express');
 const router = express.Router();
 const postController = require('../controllers/postController');
 
+router.delete('/deleteAll', postController.deleteAllPosts);
 router.get('/', postController.getAllPosts);
 router.get('/:id', postController.getPostById);
 router.post('/', postController.createPost);
 router.put('/:id', postController.updatePost);
 router.delete('/:id', postController.deletePost);
-router.delete('/deleteAll', postController.deleteAllPosts);
+
 
 module.exports = router;
